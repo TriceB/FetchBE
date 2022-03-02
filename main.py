cb7ee7eba9196ce048f3c21bafbb3844d7f7d1e8
Mon'Trice Brightmon <mnbrightmon@gmail.com>
2022-03-01 19:06:42 -0600

    - write function for spend_points()

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ But, our accounting team does care how the points are
 spent. There are two rules for determining what points to "spend" first:
 
 ● We want the oldest points to be spent first (oldest based on transaction timestamp,
-not the order they’re received)
+not the order they're received)
 ● We want no payer's points to go negative.
 
 We expect your web service to
@@ -22,11 +22,12 @@ Provide routes that:
 
 Note:
 ● We are not defining specific requests/responses. Defining these is part of the exercise.
-● We don’t expect you to use any durable data store. Storing transactions in memory is acceptable for the exercise.
+● We don't expect you to use any durable data store. Storing transactions in memory is acceptable for the exercise.
 
 """
 from datetime import datetime
 from pprint import pprint
+import collections
 
 # TODO: write a function that takes in transactions to create a list like the below - call the route in API
 payer_transactions = [{"payer": "DANNON", "points": 1000, "timestamp": "2020-11-02T14:00:00Z"},
@@ -39,10 +40,18 @@ payer_transactions = [{"payer": "DANNON", "points": 1000, "timestamp": "2020-11-
 # the oldest transaction should be the first transaction so when iterating through the list later
 # it will begin with the oldest set of points by default
 payer_transactions.sort(key=lambda date: datetime.strptime(date["timestamp"], '%Y-%m-%dT%H:%M:%SZ'))
-pprint(payer_transactions)
+# pprint(payer_transactions)
+
+
+def main():
+    spend_points(5000)
+    get_points_balance()
+
 
 # TODO: write a function to spend points that takes in the number of points to be spent
 #  - call the route in API - use a submit/POST request to send the data to the API
+
+payer_balances = []
 
 
 def spend_points(points_to_spend):
@@ -54,18 +63,99 @@ def spend_points(points_to_spend):
     Once the total for that transaction has been met, move to the next transaction.
     This continues until the total points to spend is reached,
     keeping track of how many total points were spent by each payer to update the points balance.
-    
+
     :param points_to_spend:
     :return: a new list of points spent for each payer
     """
     # points cannot go into the negative so only spend points if the total is greater than 0
+    # if points in a transaction reaches 0, stop spending points
     # if total equals 0, we want to stop spending points to prevent going negative
-   
-        
+
+    for transaction in payer_transactions:
+        # if points to spend is greater than transaction points,
+        # only spend the transaction points
+        payer = transaction["payer"]
+        point = transaction["points"]
+        if points_to_spend > 0:
+
+            if points_to_spend >= point:
+                # print(f" points spent by {transaction['payer']} = {point}")
+                points_to_spend = abs(point - points_to_spend)
+                # print(f" new total is {points_to_spend} and {transaction['payer']} = {point}")
+                payer_balances.append({transaction['payer']: -point})
+            else:
+                # if the transaction points are greater than the amount of points to spend,
+                # only spend the points to spend points
+                # print(f" {transaction['payer']} = {point} points to spend greater than total. Only spend {points_to_spend}  ")
+                payer_balances.append({payer: -points_to_spend})
+                points_to_spend = points_to_spend - point
+                # print(f" new total is {abs(points_to_spend)} and {transaction['payer']} balance is = {abs(points_to_spend)}")
+        # else:
+        #     balance.append({payer: point})
+
+    # add the payer and total spent to new empty balance list
+    # if a payer does not exist in balance, append the payer/points to balance
+        # for payer_balance in balance:
+        # if not any(transaction['payer'] == balance[0]["payer"] in keys for keys in balance):
+        # if payer_balance not in balance:
+        # # if 'DANNON' not in payer["payer"]:
+        #     print(transaction['payer'],  transaction['points'])
+        #     # payer_balance[payer] = point
+        #     balance.append([payer, point])
+
+        # # if a payer is already in the balance list, do not append it again,
+        # # instead, sum the points and update the value for that payer
+        # else:
+        #     print(f"---balance payer {transaction['payer']} is already in balance, point = {point}")
+        #     # point += point
+        #     # balance.append(point)
+        #     print(f"balance to update ---- {transaction['payer']}")
+
+    pprint(payer_balances)
+
 
 def get_points_balance():
     """
     Function to calculate the balance remaining for each payer after points have been spent
     :return: dictionary with the new balance for each payer
-    """
 
+    counter = collections.Counter()
+for d in ini_dict:
+    counter.update(d)
+
+result = dict(counter)
+    """
+    payer_balance = collections.Counter()
+    for points in payer_balances:
+        payer_balance.update(points)
+
+    result = dict(payer_balance)
+    print(f'the result is = {result}')
+
+    for transaction in payer_transactions:
+        for balance in payer_balances:
+            # if transaction['payer'] == balance.keys():
+                print(transaction['payer'], balance.keys())
+
+
+"""
+Expected Result
+[
+	{ "payer": "DANNON", "points": -100 },
+	{ "payer": "UNILEVER", "points": -200 },
+	{ "payer": "MILLER COORS", "points": -4,700 }
+]
+
+Balance Result
+{
+"DANNON": 1000,
+"UNILEVER": 0,
+"MILLER COORS": 5300
+}
+
+"""
+
+
+if __name__ == "__main__":
+    main()
+    
